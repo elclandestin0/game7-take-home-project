60d192fc366ad147a9bce6a8ec119638f7055941
cherrypopsicle <majd.khoury93@gmail.com>
2024-06-21 10:51:42 +0100

    init cards

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,12 +1,30 @@
-import { Text } from "@chakra-ui/react";
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 const GameBoard = () => {
-  useEffect(() => {
-    console.log("GameBoard loaded!");
-  }, []);
+  const [level, setLevel] = useState({ row: 5, column: 6 });
+  const [cards, setCards] = useState([]);
 
-  return <Text fontSize="xl">GameBoard</Text>;
+  useEffect(() => {
+    const totalCards = level.row * level.column;
+    const newCards = [];
+    for (let i = 0; i < totalCards; i++) {
+      newCards.push(<Box bg="tomato" height="150px" width="100px"></Box>);
+      console.log("pushed a card");
+    }
+    setCards(cards);
+  }, [level]);
+
+  return (
+    <SimpleGrid
+      columns={level.column}
+      row={level.row}
+      spacingX="80px"
+      spacingY="20px"
+    >
+      {cards}
+    </SimpleGrid>
+  );
 };
 
 export default GameBoard;
